b8d203017ea9f2c604b5fac2f37c478f974388d4
Utkarsh Rawat <debugomega@gmail.com>
2026-06-06 13:27:27 +0530

    svg digit centred

--- STATUS_CODE.md
+++ STATUS_CODE.md
@@ -1,2 +1,1 @@
-269 - visitor already in database 
 288 - unable to fetch ip of visitor 
--- src/controller/badge.controller.ts
+++ src/controller/badge.controller.ts
@@ -31,35 +31,96 @@ async function getBadge(req: Request,res: Response) {
     throw new Error (`unable to create or update userstats , error recieved - ${err}`);
 };
 
-const svg = `<svg xmlns="http://www.w3.org/2000/svg" width="108" height="20" role="img" aria-label="Views:{}: you like">
+    const svg = `
+<svg xmlns="http://www.w3.org/2000/svg" width="108" height="20" role="img" aria-label="Views: ${views}">
     <title>ViewsBadge</title>
+
     <filter id="blur">
-        <feGaussianBlur in="SourceGraphic" stdDeviation="16" />
+        <feGaussianBlur in="SourceGraphic" stdDeviation="0.8" />
     </filter>
+
     <linearGradient id="s" x2="0" y2="100%">
         <stop offset="0" stop-color="#bbb" stop-opacity=".1" />
         <stop offset="1" stop-opacity=".1" />
     </linearGradient>
+
     <clipPath id="r">
         <rect width="108" height="20" rx="3" fill="#fff" />
     </clipPath>
+
     <g clip-path="url(#r)">
         <rect width="55" height="20" fill="#555" />
         <rect x="55" width="53" height="20" fill="#007ec6" />
         <rect width="108" height="20" fill="url(#s)" />
     </g>
-    <g fill="#fff" text-anchor="middle" font-family="Verdana,Geneva,DejaVu Sans,sans-serif"
-        text-rendering="geometricPrecision" font-size="110"><text aria-hidden="true" x="285" y="150" fill="#010101"
-            fill-opacity=".80" filter="url(#blur)" transform="scale(.1)" textLength="450">${views}</text><text
-            aria-hidden="true" x="285" y="150" fill="#010101" fill-opacity=".3" transform="scale(.1)"
-            textLength="450">${views}</text><text x="285" y="140" transform="scale(.1)" fill="#fff" textLength="450">${views}
-            </text><text aria-hidden="true" x="805" y="150" fill="#010101" fill-opacity=".80" filter="url(#blur)"
-            transform="scale(.1)" textLength="430">ViewMap</text><text aria-hidden="true" x="805" y="150"
-            fill="#010101" fill-opacity=".3" transform="scale(.1)" textLength="430">ViewMap</text><text x="805" y="140"
-            transform="scale(.1)" fill="#fff" textLength="430">ViewMap</text></g>
-</svg>`
 
+    <g
+        fill="#fff"
+        text-anchor="middle"
+        font-family="Verdana,Geneva,DejaVu Sans,sans-serif"
+        text-rendering="geometricPrecision"
+        font-size="110"
+    >
 
+        <!-- Views count shadow -->
+        <text
+            aria-hidden="true"
+            x="275"
+            y="150"
+            fill="#010101"
+            fill-opacity=".8"
+            filter="url(#blur)"
+            transform="scale(.1)"
+        >${views}</text>
+
+        <text
+            aria-hidden="true"
+            x="275"
+            y="150"
+            fill="#010101"
+            fill-opacity=".3"
+            transform="scale(.1)"
+        >${views}</text>
+
+        <!-- Views count -->
+        <text
+            x="275"
+            y="140"
+            transform="scale(.1)"
+            fill="#fff"
+        >${views}</text>
+
+        <!-- ViewMap shadow -->
+        <text
+            aria-hidden="true"
+            x="815"
+            y="150"
+            fill="#010101"
+            fill-opacity=".8"
+            filter="url(#blur)"
+            transform="scale(.1)"
+        >ViewMap</text>
+
+        <text
+            aria-hidden="true"
+            x="815"
+            y="150"
+            fill="#010101"
+            fill-opacity=".3"
+            transform="scale(.1)"
+        >ViewMap</text>
+
+        <!-- ViewMap -->
+        <text
+            x="815"
+            y="140"
+            transform="scale(.1)"
+            fill="#fff"
+        >ViewMap</text>
+
+    </g>
+</svg>
+`;
 
 try{                                                      // storing the data of visitor
 
